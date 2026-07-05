1b3b0f41b8a1d63c785ecae61eef9b29f89a1d67
ElsterQA <elster.anton@gmail.com>
2026-07-05 07:14:30 +0300

    sandBoxPC1

--- Udemy_JS/My_example.spec.js
+++ Udemy_JS/My_example.spec.js
@@ -6,17 +6,18 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Prom – найбільший маркетплейс України/);
-  
-  await page.getByRole('link', { name: 'Почати продавати' }).first().click();
 
+  await page.locator('a[href="/sell-on-prom?source_id=banner.b.main"]').click();
+ // await page.getByRole('link', { name: 'Почати продавати' }).first().click();
+await expect(page).toHaveURL('https://prom.ua/sell-on-prom?source_id=banner.b.main');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+//test('get started link', async ({ page }) => {
+  //await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+ // await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  //await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+//});
