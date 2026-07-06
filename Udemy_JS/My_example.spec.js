52abe4206ecb07c9579912d7c1c982f5c54b5699
ElsterQA <elster.anton@gmail.com>
2026-07-06 14:17:46 +0300

    sandBoxPC2

--- Udemy_JS/My_example.spec.js
+++ Udemy_JS/My_example.spec.js
@@ -2,14 +2,15 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://prom.ua/');
-
+  await page.goto('http://localhost:2221/');
+await expect(page).toHaveURL('http://localhost:2221/');
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Prom – найбільший маркетплейс України/);
 
   await page.locator('a[href="/sell-on-prom?source_id=banner.b.main"]').click();
  // await page.getByRole('link', { name: 'Почати продавати' }).first().click();
-await expect(page).toHaveURL('https://prom.ua/sell-on-prom?source_id=banner.b.main');
+
+await expect(page.getByText('Регистрация')).toBeVisible();
 });
 
 //test('get started link', async ({ page }) => {
