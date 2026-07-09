4a1d910419a869a39499143a5db12eae0bc76419
ElsterQA <elster.anton@gmail.com>
2026-07-09 15:57:33 +0300

    PC_Less8

--- Udemy_JS/Less8.spec.js
+++ Udemy_JS/Less8.spec.js
@@ -5,7 +5,7 @@ await page.goto('http://localhost:2221/')
 
 
 const addToBasketButton = page.getByRole('button', { name: 'Add to Basket' }).first() // Если в строке есть методы которые возвращают локатор=(НЕ возвращают Промис), то в этой же строке(в самомо начале) НЕ указываем await
-await addToBasketButton.waitFor()//.waitFor()- в д.с. ждет пока элемент указаный више, отобразится на странице
+await addToBasketButton.waitFor()//.waitFor()- в д.с. ждет пока элемент указаный више(кнопка с названием 'Add to Basket' ) отобразится на странице
 await addToBasketButton.click()// Если в строке есть методы которые возвращают Промис, то в этой же строке(в самомо начале) указываем await
 
  
