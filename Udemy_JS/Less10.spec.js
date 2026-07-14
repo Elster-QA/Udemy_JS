2c0edf6554fc4da951b43d2d277cf8e6af418870
ElsterQA <elster.anton@gmail.com>
2026-07-14 16:02:08 +0300

    PC_Less10

--- Udemy_JS/Less10.spec.js
+++ Udemy_JS/Less10.spec.js
@@ -7,6 +7,8 @@ await page.goto('http://localhost:2221/')
 
 const addToBasketButton = page.locator('[data-qa="product-button"]').first()//В 8 ом уроке тут был getByRole, но он был заменен потому как getByRole использует ИМЯ элемента, а оно изменяется при клике, поэтому используме CSS-селектор(он при клике не изменный) 
 const basketCounter = page.locator('[data-qa="header-basket-count"]')//переменная счетчика корзины
+const CheckOut = page.locator('[data-qa="desktop-nav-link"]').nth(2)//+
+const CheckEl = page.locator('[data-qa="basket-card"]')//+
 await addToBasketButton.waitFor()//.waitFor()- в д.с. ждет пока элемент указаный више(кнопка с названием 'Add to Basket' ) отобразится на странице
 await expect(basketCounter).toHaveText('0')//проверяем что счетчик корзины имеет знач. "0"
 await expect(addToBasketButton).toHaveText('Add to Basket')//проверяем что данная кнопка иммет название "Add to Basket"
@@ -16,9 +18,14 @@ await expect(addToBasketButton).toHaveText('Remove from Basket')
 
 await expect(basketCounter).toHaveText('1') 
 
+await CheckOut.click()//+
+await expect(page).toHaveURL('http://localhost:2221/basket')//+
+await expect(CheckEl).toBeVisible()//+
+
+
+
 
 
  
- 
 
-});
+})
