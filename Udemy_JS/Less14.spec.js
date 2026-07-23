4a1fb8cf5515183762d32e2154012c889cc7d701
ElsterQA <elster.anton@gmail.com>
2026-07-23 16:06:33 +0300

    PC_Less14

---
+++ Udemy_JS/Less14.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test'
+import { ProductsPage } from '../page-objects/ProductPage_14'
+
+test('New user full end-to-end test journey', async ({ page }) => {
+  const productsPage = new ProductsPage(page)
+  await productsPage.visit()
+  
+ await productsPage.addProductToBasket(0)
+ await productsPage.addProductToBasket(1)
+ await productsPage.addProductToBasket(2)
+
+
+
+  // await page.pause()
+
+})
---
+++ page-objects/ProductPage_14.js
@@ -0,0 +1,27 @@
+import { expect } from '@playwright/test'
+
+export class ProductsPage {
+    constructor(page) {
+        this.page = page
+        this.addButtons = this.page.locator('[data-qa="product-button"]')
+        
+    }
+
+    visit = async () => {
+        await this.page.goto('/')
+    }
+
+    addProductToBasket = async (index) => {
+        await this.addButtons.nth(index).waitFor()
+        await this.addButtons.nth(index).click()
+        
+    }
+
+   
+
+
+
+
+
+
+}
