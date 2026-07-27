ab9465a1b608735a1790430aa000f605cb512bb7
ElsterQA <elster.anton@gmail.com>
2026-07-27 14:50:22 +0300

    PC_Less15

---
+++ page-objects/ProductPage_15.js
@@ -0,0 +1,36 @@
+import { expect } from '@playwright/test'
+
+export class ProductsPage {
+    constructor(page) {
+        this.page = page
+        this.addButtons = this.page.locator('[data-qa="product-button"]')
+        
+               
+    }
+
+    visit = async () => {
+        await this.page.goto('/')
+    }
+
+    addProductToBasket = async (index) => {
+        const specificAddButton = this.addButtons.nth(index)//Создали переменную для "красоты" кода 
+
+        await specificAddButton.waitFor()
+       expect (specificAddButton).toHaveText('Add to Basket')//+
+        await specificAddButton.click()
+       expect(specificAddButton).toHaveText('Remove from Basket')//+
+        
+    }
+
+   
+
+   
+
+   
+
+
+
+
+
+
+}
